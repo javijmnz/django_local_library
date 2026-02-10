78848925deac7fb03ae404757694566980012e80
jpc03 <janirapua03@gmail.com>
2026-02-10 18:14:37 +0100

    Completed part 5 of the tutorial.
    Co-authored-by: Javier Jimenez Garcia <javijmnz@users.noreply.github.com>

--- catalog/views.py
+++ catalog/views.py
@@ -15,12 +15,23 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Genres and books with a specific word
+    genres_word = 'Fiction'
+    num_genres_word = Genre.objects.filter(name__icontains=genres_word).count()
+
+    books_word = 'the'
+    num_books_word = Book.objects.filter(title__icontains=books_word).count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'genres_word': genres_word,
+        'num_genres_word': num_genres_word,
+        'books_word': books_word,
+        'num_books_word': num_books_word,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context=contex

10626396ec6740bb4539d9fe1652d58dccc06dec
jpc03 <janirapua03@gmail.com>
2026-02-15 19:21:16 +0100

    Allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['p1-07-2401-2026-1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
